1f23b92ec6248d4c2eb42e4655ec73a9bb6c3b1d
Чепко Михаил Николаевич - ПВБ <Chepko-MN@sigma.sbrf.ru>
2019-10-31 19:36:23 +0300

    Updates

---
+++ precache-manifest.0e40f27fe7e6f1ddcc1a5d5a5b694250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c814ca97c801676eaf0ad95a7c882ad",
+    "url": "/weighted_poll/index.html"
+  },
+  {
+    "revision": "c49f8479f7c3e01fcb5a",
+    "url": "/weighted_poll/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c415214d33aea3a38596",
+    "url": "/weighted_poll/static/js/2.3e943eda.chunk.js"
+  },
+  {
+    "revision": "c49f8479f7c3e01fcb5a",
+    "url": "/weighted_poll/static/js/main.24e09c9e.chunk.js"
+  },
+  {
+    "revision": "f3d7843e43bf1b60935a",
+    "url": "/weighted_poll/static/js/runtime-main.ed9ccb0c.js"
+  }
+]);
